f2365d662242868cf98d897e2ae19e0e4674aa5a
Wajaja <cedrickngeja@gmail.com>
2025-04-18 12:55:50 -0400

    first part2

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
-
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
 
@@ -23,8 +22,19 @@ class LessonAdmin(admin.ModelAdmin):
 
 
 # <HINT> Register Question and Choice models here
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -115,10 +115,12 @@ class Question(models.Model):
             return True
         else:
             return False
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
+
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
